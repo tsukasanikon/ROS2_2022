58ecb4052a2e5d5862c115356f58d0d99597e535
tsukasanikon <tsukasa.momma@nikonlenswear.com>
2022-10-11 13:36:30 +0900

    add other error examples

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv 
+#!/bin/bash  
 # SPDX-FileCopyrightText: 2022 Tsukasa Momma
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -12,6 +12,15 @@ res=0
 ### I/O TEST ###
 out=$(seq 5 | ./plus) 
 [ "${out}" = 15 ] || ng $LINENO
+
+### Strange input ###
+out=$(echo あ | ./plus)
+[ "$?" = 1      ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(echo  | ./plus)
+[ "$?" = 1      ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
 exit $res
