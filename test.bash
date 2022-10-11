e88f28a3f514eb0b58ae3a7175dde8b23de037c9
tsukasanikon <tsukasa.momma@nikonlenswear.com>
2022-10-11 13:24:50 +0900

    add test and -xv option

--- test.bash
+++ test.bash
@@ -1,3 +1,17 @@
-#!/bin/bash
+#!/bin/bash -xv 
+# SPDX-FileCopyrightText: 2022 Tsukasa Momma
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG as Line $1
+	res=1
+}	
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus) 
+[ "${out}" = 15 ] || ng $LINENO
+
+[ "$res" = 0 ] && echo OK
+exit $res
