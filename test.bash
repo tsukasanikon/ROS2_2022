a315a522ccfebc432e081a41b2a60c654472dbad
tsukasanikon <tsukasa.momma@nikonlenswear.com>
2022-10-11 16:30:32 +0900

    some copy from ueda's site

--- test.bash
+++ test.bash
@@ -1,9 +1,9 @@
-#!/bin/bash  
+#!/bin/bash  -xv
 # SPDX-FileCopyrightText: 2022 Tsukasa Momma
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo NG as Line $1
+	echo NG at Line $1
 	res=1
 }	
 
@@ -11,16 +11,16 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus) 
-[ "${out}" = 15 ] || ng $LINENO
+[ "${out}" = 15 ] || ng ${LINENO}
 
 ### Strange input ###
 out=$(echo あ | ./plus)
-[ "$?" = 1      ] || ng $LINENO
-[ "${out}" = "" ] || ng $LINENO
+[ "$?" = 1      ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo  | ./plus)
-[ "$?" = 1      ] || ng $LINENO
-[ "${out}" = "" ] || ng $LINENO
+[ "$?" = 1      ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
